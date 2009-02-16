7f394a12197b1b6eca9b588b19e8b00c262beec0
the.stuart.sierra <the.stuart.sierra@41958a8e-ca1b-11dd-86d7-074df07e0730>
2009-02-16 17:19:59 +0000

    Added branches/lazy, to track compatibility with Clojure's lazy branch.

---
+++ branches/lazy/src/clojure/contrib/accumulators.clj
@@ -0,0 +1,263 @@
+;; Accumulators
+
+;; by Konrad Hinsen
+;; last updated February 15, 2009
+
+;; This module defines various accumulators (list, vector, map,
+;; sum, product, counter, and combinations thereof) with a common
+;; interface defined by the multimethods add and combine.
+;; For each accumulator type, its empty value is defined in this module.
+;; Applications typically use this as a starting value and add data
+;; using the add multimethod.
+
+;; Copyright (c) Konrad Hinsen, 2009. All rights reserved.  The use
+;; and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this
+;; distribution.  By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license.  You must not
+;; remove this notice, or any other, from this software.
+
+(ns clojure.contrib.accumulators
+  (:use [clojure.contrib.macros :only (letfn)])
+  (:use [clojure.contrib.def :only (defvar defvar- defmacro-)]))
+
+(defn- selector
+  [& vs]
+  (let [fv (first vs)
+	tag (get ^fv ::accumulator nil)]
+    (if (nil? tag) (class fv) tag)))
+
+(defmulti
+  #^{:doc "Add item to the accumulator acc. The exact meaning of adding an
+           an item depends on the type of the accumulator."
+     :arglists '([acc item])}
+  add selector)
+
+(defn add-items
+  "Add all elements of a collection coll to the accumulator acc."
+  [acc items]
+  (reduce add acc items))
+
+(defmulti
+  #^{:doc "Combine the values of the accumulators acc1 and acc2 into a
+           single accumulator of the same type."
+     :arglists '([acc1 acc2])}
+  combine selector)
+
+
+;
+; Vector accumulator
+;
+(defvar empty-vector []
+  "An empty vector accumulator. Adding an item appends it at the end.")
+
+(defmethod combine clojure.lang.IPersistentVector
+  [& vs]
+  (vec (apply concat vs)))
+
+(defmethod add clojure.lang.IPersistentVector
+  [v e]
+  (conj v e))
+
+;
+; List accumulator
+;
+(defvar empty-list '()
+  "An empty list accumulator. Adding an item appends it at the beginning.")
+
+(defmethod combine clojure.lang.IPersistentList
+  [& vs]
+  (apply concat vs))
+
+(defmethod add clojure.lang.IPersistentList
+  [v e]
+  (conj v e))
+
+;
+; Queue accumulator
+;
+(defvar empty-queue clojure.lang.PersistentQueue/EMPTY
+  "An empty queue accumulator. Adding an item appends it at the end.")
+
+(defmethod combine clojure.lang.PersistentQueue
+  [& vs]
+  (add-items (first vs) (apply concat (rest vs))))
+
+(defmethod add clojure.lang.PersistentQueue
+  [v e]
+  (conj v e))
+
+;
+; Set accumulator
+;
+(defvar empty-set #{}
+  "An empty set accumulator.")
+
+; A multi-argument version of set/union
+(defn- union
+  [set & sets]
+  (reduce clojure.set/union set sets))
+
+(defmethod combine (class empty-set)
+  [& vs]
+  (apply union vs))
+
+(defmethod add (class empty-set)
+  [v e]
+  (conj v e))
+
+;
+; String accumulator
+;
+(defvar empty-string ""
+  "An empty string accumulator. Adding an item (string or character)
+   appends it at the end.")
+
+(defmethod combine java.lang.String
+  [& vs]
+  (apply str vs))
+
+(defmethod add java.lang.String
+  [v e]
+  (str v e))
+
+;
+; Map accumulator
+;
+(defvar empty-map {}
+  "An empty map accumulator. Items to be added must be [key value] pairs.")
+
+(defmethod combine clojure.lang.IPersistentMap
+  [& vs]
+  (apply merge vs))
+
+(defmethod add clojure.lang.IPersistentMap
+  [v e]
+  (conj v e))
+
+;
+; Numerical accumulators: sum, product, minimum, maximum
+;
+(defmacro- defacc
+  [name op empty doc-string]
+  (let [struct-tag (keyword (str name))
+	meta-tag (keyword (str *ns*) (str name))
+	empty-symbol (symbol (str "empty-" name))]
+  `(let [op# ~op
+	 meta-data# {::accumulator ~meta-tag}
+	 struct-basis# (create-struct ~struct-tag)
+	 get-value# (accessor struct-basis# ~struct-tag)
+	 make-fn# (fn [n#] (with-meta (struct struct-basis# n#) meta-data#))]
+     (defvar ~empty-symbol (make-fn# ~empty) ~doc-string)
+     (defmethod combine ~meta-tag [& vs#]
+       (make-fn# (apply op# (map get-value# vs#))))
+     (defmethod add ~meta-tag [v# e#]
+       (make-fn# (op# (get-value# v#) e#))))))
+
+(defacc sum + 0
+  "An empty sum accumulator. Only numbers can be added.")
+
+(defacc product * 1
+  "An empty sum accumulator. Only numbers can be added.")
+
+; The empty maximum accumulator should have value -infinity.
+; This is represented by nil and taken into account in an
+; adapted max function. In the minimum accumulator, nil is
+; similarly used to represent +infinity.
+
+(defacc maximum (fn [& xs]
+		  (when-let [xs (seq (filter identity xs))]
+		      (apply max xs)))
+                nil
+  "An empty maximum accumulator. Only numbers can be added.")
+
+(defacc minimum (fn [& xs]
+		  (when-let [xs (seq (filter identity xs))]
+		      (apply min xs)))
+                nil
+  "An empty minimum accumulator. Only numbers can be added.")
+
+;
+; Numeric range accumulator
+; (combination of minimum and maximum)
+;
+(let [range-tag {::accumulator ::range}]
+  (defn- make-range
+    [min max]
+    (with-meta {:minimum min :maximum max} range-tag)))
+
+(defvar empty-range (make-range nil nil)
+  "An empty range accumulator, combining minimum and maximum.
+   Only numbers can be added.")
+
+(defmethod combine ::range
+  [& vs]
+  (let [total-min (apply min (map :minimum vs))
+	total-max (apply max (map :maximum vs))]
+    (make-range total-min total-max)))
+
+(defmethod add ::range
+  [v e]
+  (let [{min-v :minimum max-v :maximum} v
+	new-min (if (nil? min-v) e (min min-v e))
+	new-max (if (nil? max-v) e (max max-v e))]
+    (make-range new-min new-max)))
+
+;
+; Counter accumulator
+;
+(defvar empty-counter (with-meta {} {::accumulator ::counter})
+  "An empty counter accumulator. Its value is a map that stores for
+   every item the number of times it was added.")
+
+(defmethod combine ::counter
+  [v & vs]
+  (letfn [add-item [counter [item n]]
+	    (assoc counter item (+ n (get counter item 0)))
+	  add-two [c1 c2] (reduce add-item c1 c2)]
+    (reduce add-two v vs)))
+
+(defmethod add ::counter
+  [v e]
+  (assoc v e (inc (get v e 0))))
+
+;
+; Counter accumulator with total count
+;
+(derive ::counter-with-total ::counter)
+
+(defvar empty-counter-with-total
+  (with-meta {:total 0} {::accumulator ::counter-with-total})
+  "An empty counter-with-total accumulator. It works like the counter
+   accumulator, except that the total number of items added is stored as the
+   value of the key :totall.")
+
+(defmethod add ::counter-with-total
+  [v e]
+  (assoc v e (inc (get v e 0))
+           :total (inc (:total v))))
+
+;
+; Accumulator n-tuple
+;
+(let [tuple-tag {::accumulator ::tuple}]
+  (defn- make-tuple
+    [seq]
+    (with-meta (vec seq) tuple-tag)))
+
+(defn empty-tuple
+  "Returns an accumulator tuple with the supplied empty-accumulators
+   as its value. Accumulator tuples consist of several accumulators that
+   work in parallel. Added items must be sequences whose number of elements
+   matches the number of sub-accumulators."
+  [empty-accumulators]
+  (make-tuple empty-accumulators))
+
+(defmethod combine ::tuple
+  [& vs]
+  (make-tuple (map combine vs)))
+
+(defmethod add ::tuple
+  [v e]
+  (make-tuple (map add v e)))
---
+++ branches/lazy/src/clojure/contrib/test_clojure/sequences.clj
@@ -0,0 +1,634 @@
+;;  Copyright (c) Frantisek Sodomka. All rights reserved.  The use and
+;;  distribution terms for this software are covered by the Eclipse Public
+;;  License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;  be found in the file epl-v10.html at the root of this distribution.  By
+;;  using this software in any fashion, you are agreeing to be bound by the
+;;  terms of this license.  You must not remove this notice, or any other,
+;;  from this software.
+;;
+
+(ns clojure.contrib.test-clojure.sequences
+  (:use clojure.contrib.test-is))
+
+;; *** Helper functions ***
+
+(defn exception []
+  (throw (new Exception "Exception which should never occur")))
+
+(defn diff [s1 s2]
+  (seq (reduce disj (set s1) (set s2))))
+
+
+;; *** Tests ***
+
+(deftest test-seq
+  (are (= _1 _2)
+    (seq nil) nil
+    (seq [nil]) '(nil)
+
+    (seq ()) nil
+    (seq []) nil
+    (seq #{}) nil
+    (seq {}) nil
+    (seq "") nil
+    (seq (into-array [])) nil
+
+    (seq (list 1 2)) '(1 2)
+    (seq [1 2]) '(1 2)
+    (seq (sorted-set 1 2)) '(1 2)
+    (seq (sorted-map :a 1 :b 2)) '([:a 1] [:b 2])
+    (seq "abc") '(\a \b \c)
+    (seq (into-array [1 2])) '(1 2) ))
+
+
+(deftest test-cons
+  (is (thrown? IllegalArgumentException (cons 1 2)))
+  (are (= _1 _2)
+    (cons 1 nil) '(1)
+    (cons nil nil) '(nil)
+
+    (cons \a nil) '(\a)
+    (cons \a "") '(\a)
+    (cons \a "bc") '(\a \b \c)
+
+    (cons 1 ()) '(1)
+    (cons 1 '(2 3)) '(1 2 3)
+
+    (cons 1 []) [1]
+    (cons 1 [2 3]) [1 2 3]
+
+    (cons 1 #{}) '(1)
+    (cons 1 (sorted-set 2 3)) '(1 2 3)
+
+    (cons 1 (into-array [])) '(1)
+    (cons 1 (into-array [2 3])) '(1 2 3) ))
+
+
+(deftest test-first
+  (is (thrown? IllegalArgumentException (first)))
+  (is (thrown? IllegalArgumentException (first true)))
+  (is (thrown? IllegalArgumentException (first false)))
+  (is (thrown? IllegalArgumentException (first 1)))
+  (is (thrown? IllegalArgumentException (first 1 2)))
+  (is (thrown? IllegalArgumentException (first \a)))
+  (is (thrown? IllegalArgumentException (first 's)))
+  (is (thrown? IllegalArgumentException (first :k)))
+  (are (= _1 _2)
+    (first nil) nil
+
+    ; string
+    (first "") nil
+    (first "a") \a
+    (first "abc") \a
+
+    ; list
+    (first ()) nil
+    (first '(1)) 1
+    (first '(1 2 3)) 1
+
+    (first '(nil)) nil
+    (first '(1 nil)) 1
+    (first '(nil 2)) nil
+    (first '(())) ()
+    (first '(() nil)) ()
+    (first '(() 2 nil)) ()
+
+    ; vector
+    (first []) nil
+    (first [1]) 1
+    (first [1 2 3]) 1
+
+    (first [nil]) nil
+    (first [1 nil]) 1
+    (first [nil 2]) nil
+    (first [[]]) []
+    (first [[] nil]) []
+    (first [[] 2 nil]) []
+
+    ; set
+    (first #{}) nil
+    (first #{1}) 1
+    ;(first #{1 2 3}) 1
+
+    (first #{nil}) nil
+    ;(first #{1 nil}) 1
+    ;(first #{nil 2}) nil
+    (first #{#{}}) #{}
+    ;(first #{#{} nil}) #{}
+    ;(first #{#{} 2 nil}) #{}
+
+    ; map
+    (first {}) nil
+    (first (sorted-map :a 1)) '(:a 1)
+    (first (sorted-map :a 1 :b 2 :c 3)) '(:a 1)
+
+    ; array
+    (first (into-array [])) nil
+    (first (into-array [1])) 1
+    (first (into-array [1 2 3])) 1
+    (first (to-array [nil])) nil
+    (first (to-array [1 nil])) 1
+    (first (to-array [nil 2])) nil ))
+
+
+(deftest test-rest
+  (is (thrown? IllegalArgumentException (rest)))
+  (is (thrown? IllegalArgumentException (rest true)))
+  (is (thrown? IllegalArgumentException (rest false)))
+  (is (thrown? IllegalArgumentException (rest 1)))
+  (is (thrown? IllegalArgumentException (rest 1 2)))
+  (is (thrown? IllegalArgumentException (rest \a)))
+  (is (thrown? IllegalArgumentException (rest 's)))
+  (is (thrown? IllegalArgumentException (rest :k)))
+  (are (= _1 _2)
+    (rest nil) nil
+
+    ; string
+    (rest "") nil
+    (rest "a") nil
+    (rest "abc") '(\b \c)
+
+    ; list
+    (rest ()) nil
+    (rest '(1)) nil
+    (rest '(1 2 3)) '(2 3)
+
+    (rest '(nil)) nil
+    (rest '(1 nil)) '(nil)
+    (rest '(1 ())) '(())
+    (rest '(nil 2)) '(2)
+    (rest '(())) nil
+    (rest '(() nil)) '(nil)
+    (rest '(() 2 nil)) '(2 nil)
+
+    ; vector
+    (rest []) nil
+    (rest [1]) nil
+    (rest [1 2 3]) [2 3]
+
+    (rest [nil]) nil
+    (rest [1 nil]) [nil]
+    (rest [1 []]) [[]]
+    (rest [nil 2]) [2]
+    (rest [[]]) nil
+    (rest [[] nil]) [nil]
+    (rest [[] 2 nil]) [2 nil]
+
+    ; set
+    (rest #{}) nil
+    (rest #{1}) nil
+    ;(rest #{1 2 3}) 1
+
+    (rest #{nil}) nil
+    ;(rest #{1 nil}) 1
+    ;(rest #{nil 2}) nil
+    (rest #{#{}}) nil
+    ;(rest #{#{} nil}) #{}
+    ;(rest #{#{} 2 nil}) #{}
+
+    ; map
+    (rest {}) nil
+    (rest (sorted-map :a 1)) nil
+    (rest (sorted-map :a 1 :b 2 :c 3)) '((:b 2) (:c 3))
+
+    ; array
+    (rest (into-array [])) nil
+    (rest (into-array [1])) nil
+    (rest (into-array [1 2 3])) '(2 3)
+
+    (rest (to-array [nil])) nil
+    (rest (to-array [1 nil])) '(nil)
+    ;(rest (to-array [1 (into-array [])])) (list (into-array []))
+    (rest (to-array [nil 2])) '(2)
+    (rest (to-array [(into-array [])])) nil
+    (rest (to-array [(into-array []) nil])) '(nil)
+    (rest (to-array [(into-array []) 2 nil])) '(2 nil) ))
+
+
+;; (ffirst coll) = (first (first coll))
+;;
+(deftest test-ffirst
+  (is (thrown? IllegalArgumentException (ffirst)))
+  (are (= _1 _2)
+    (ffirst nil) nil
+
+    (ffirst ()) nil
+    (ffirst '((1 2) (3 4))) 1
+
+    (ffirst []) nil
+    (ffirst [[1 2] [3 4]]) 1
+
+    (ffirst {}) nil
+    (ffirst {:a 1}) :a
+
+    (ffirst #{}) nil
+    (ffirst #{[1 2]}) 1 ))
+
+
+;; (frest coll) = (first (rest coll)) = (second coll)
+;;
+(deftest test-frest
+  (is (thrown? IllegalArgumentException (frest)))
+  (are (= _1 _2)
+    (frest nil) nil
+
+    (frest ()) nil
+    (frest '(1)) nil
+    (frest '(1 2 3 4)) 2
+
+    (frest []) nil
+    (frest [1]) nil
+    (frest [1 2 3 4]) 2
+
+    (frest {}) nil
+    (frest (sorted-map :a 1)) nil
+    (frest (sorted-map :a 1 :b 2)) [:b 2]
+
+    (frest #{}) nil
+    (frest #{1}) nil
+    (frest (sorted-set 1 2 3 4)) 2 ))
+
+
+;; (rfirst coll) = (rest (first coll))
+;;
+(deftest test-rfirst
+  (is (thrown? IllegalArgumentException (rfirst)))
+  (are (= _1 _2)
+    (rfirst nil) nil
+
+    (rfirst ()) nil
+    (rfirst '((1 2 3) (4 5 6))) '(2 3)
+
+    (rfirst []) nil
+    (rfirst [[1 2 3] [4 5 6]]) '(2 3)
+
+    (rfirst {}) nil
+    (rfirst {:a 1}) '(1)
+
+    (rfirst #{}) nil
+    (rfirst #{[1 2]}) '(2) ))
+
+
+;; (rrest coll) = (rest (rest coll))
+;;
+(deftest test-rrest
+  (is (thrown? IllegalArgumentException (rrest)))
+  (are (= _1 _2)
+    (rrest nil) nil
+
+    (rrest ()) nil
+    (rrest '(1)) nil
+    (rrest '(1 2)) nil
+    (rrest '(1 2 3 4)) '(3 4)
+
+    (rrest []) nil
+    (rrest [1]) nil
+    (rrest [1 2]) nil
+    (rrest [1 2 3 4]) '(3 4)
+
+    (rrest {}) nil
+    (rrest (sorted-map :a 1)) nil
+    (rrest (sorted-map :a 1 :b 2)) nil
+    (rrest (sorted-map :a 1 :b 2 :c 3 :d 4)) '([:c 3] [:d 4])
+
+    (rrest #{}) nil
+    (rrest #{1}) nil
+    (rrest (sorted-set 1 2)) nil
+    (rrest (sorted-set 1 2 3 4)) '(3 4) ))
+
+
+; distinct was broken for nil & false:
+;   fixed in rev 1278:
+;   http://code.google.com/p/clojure/source/detail?r=1278
+;
+(deftest test-distinct
+  (are (= _1 _2)
+      (distinct ()) nil
+      (distinct '(1)) '(1)
+      (distinct '(1 2 3)) '(1 2 3)
+      (distinct '(1 2 3 1 1 1)) '(1 2 3)
+      (distinct '(1 1 1 2)) '(1 2)
+      (distinct '(1 2 1 2)) '(1 2)
+
+      (distinct []) nil
+      (distinct [1]) '(1)
+      (distinct [1 2 3]) '(1 2 3)
+      (distinct [1 2 3 1 2 2 1 1]) '(1 2 3)
+      (distinct [1 1 1 2]) '(1 2)
+      (distinct [1 2 1 2]) '(1 2)
+
+      (distinct "") nil
+      (distinct "a") '(\a)
+      (distinct "abc") '(\a \b \c)
+      (distinct "abcabab") '(\a \b \c)
+      (distinct "aaab") '(\a \b)
+      (distinct "abab") '(\a \b) )
+
+  (are (= (distinct [_ _]) [_])   ; (distinct [x x]) = [x]
+      nil
+      false true
+      0 42
+      0.0 3.14
+      2/3
+      0M 1M
+      \c
+      "" "abc"
+      'sym
+      :kw
+      () '(1 2)
+      [] [1 2]
+      {} {:a 1 :b 2}
+      #{} #{1 2} ))
+
+
+(deftest test-interpose
+  (are (= _1 _2)
+    (interpose 0 []) nil
+    (interpose 0 [1]) '(1)
+    (interpose 0 [1 2]) '(1 0 2)
+    (interpose 0 [1 2 3]) '(1 0 2 0 3) ))
+
+
+(deftest test-interleave
+  (are (= _1 _2)
+    (interleave [1 2] [3 4]) '(1 3 2 4)
+
+    (interleave [1] [3 4]) '(1 3)
+    (interleave [1 2] [3]) '(1 3)
+
+    (interleave [] [3 4]) nil
+    (interleave [1 2] []) nil
+    (interleave [] []) nil ))
+
+
+(deftest test-zipmap
+  (are (= _1 _2)
+    (zipmap [:a :b] [1 2]) {:a 1 :b 2}
+
+    (zipmap [:a] [1 2]) {:a 1}
+    (zipmap [:a :b] [1]) {:a 1}
+
+    (zipmap [] [1 2]) {}
+    (zipmap [:a :b] []) {}
+    (zipmap [] []) {} ))
+
+
+(deftest test-concat
+  (are (= _1 _2)
+    (concat) nil
+
+    (concat []) nil
+    (concat [1 2]) '(1 2)
+
+    (concat [1 2] [3 4]) '(1 2 3 4)
+    (concat [] [3 4]) '(3 4)
+    (concat [1 2] []) '(1 2)
+    (concat [] []) nil
+
+    (concat [1 2] [3 4] [5 6]) '(1 2 3 4 5 6) ))
+
+
+(deftest test-cycle
+  (are (= _1 _2)
+    (cycle []) nil
+
+    (take 3 (cycle [1])) '(1 1 1)
+    (take 5 (cycle [1 2 3])) '(1 2 3 1 2)
+
+    (take 3 (cycle [nil])) '(nil nil nil) ))
+
+
+(deftest test-partition
+  (are (= _1 _2)
+    (partition 2 [1 2 3]) '((1 2))
+    (partition 2 [1 2 3 4]) '((1 2) (3 4))
+    (partition 2 []) nil
+
+    (partition 2 3 [1 2 3 4 5 6 7]) '((1 2) (4 5))
+    (partition 2 3 [1 2 3 4 5 6 7 8]) '((1 2) (4 5) (7 8))
+    (partition 2 3 []) nil
+
+    (partition 1 []) nil
+    (partition 1 [1 2 3]) '((1) (2) (3))
+
+    (partition 5 [1 2 3]) nil
+
+;    (partition 0 [1 2 3]) (repeat nil)   ; infinite sequence of nil
+    (partition -1 [1 2 3]) nil
+    (partition -2 [1 2 3]) nil ))
+
+
+(deftest test-reverse
+  (are (= _1 _2)
+    (reverse []) nil
+    (reverse [1]) '(1)
+    (reverse [1 2 3]) '(3 2 1) ))
+
+
+(deftest test-take
+  (are (= _1 _2)
+    (take 1 [1 2 3 4 5]) '(1)
+    (take 3 [1 2 3 4 5]) '(1 2 3)
+    (take 5 [1 2 3 4 5]) '(1 2 3 4 5)
+    (take 9 [1 2 3 4 5]) '(1 2 3 4 5)
+
+    (take 0 [1 2 3 4 5]) nil
+    (take -1 [1 2 3 4 5]) nil
+    (take -2 [1 2 3 4 5]) nil ))
+
+
+(deftest test-drop
+  (are (= _1 _2)
+    (drop 1 [1 2 3 4 5]) '(2 3 4 5)
+    (drop 3 [1 2 3 4 5]) '(4 5)
+    (drop 5 [1 2 3 4 5]) nil
+    (drop 9 [1 2 3 4 5]) nil
+
+    (drop 0 [1 2 3 4 5]) '(1 2 3 4 5)
+    (drop -1 [1 2 3 4 5]) '(1 2 3 4 5)
+    (drop -2 [1 2 3 4 5]) '(1 2 3 4 5) ))
+
+
+(deftest test-take-nth
+  (are (= _1 _2)
+     (take-nth 1 [1 2 3 4 5]) '(1 2 3 4 5)
+     (take-nth 2 [1 2 3 4 5]) '(1 3 5)
+     (take-nth 3 [1 2 3 4 5]) '(1 4)
+     (take-nth 4 [1 2 3 4 5]) '(1 5)
+     (take-nth 5 [1 2 3 4 5]) '(1)
+     (take-nth 9 [1 2 3 4 5]) '(1)
+
+     ; infinite seq of 1s = (repeat 1)
+     ;(take-nth 0 [1 2 3 4 5])
+     ;(take-nth -1 [1 2 3 4 5])
+     ;(take-nth -2 [1 2 3 4 5])
+  ))
+
+
+(deftest test-take-while
+  (are (= _1 _2)
+    (take-while pos? []) nil
+    (take-while pos? [1 2 3 4]) '(1 2 3 4)
+    (take-while pos? [1 2 3 -1]) '(1 2 3)
+    (take-while pos? [1 -1 2 3]) '(1)
+    (take-while pos? [-1 1 2 3]) nil
+    (take-while pos? [-1 -2 -3]) nil ))
+
+
+(deftest test-drop-while
+  (are (= _1 _2)
+    (drop-while pos? []) nil
+    (drop-while pos? [1 2 3 4]) nil
+    (drop-while pos? [1 2 3 -1]) '(-1)
+    (drop-while pos? [1 -1 2 3]) '(-1 2 3)
+    (drop-while pos? [-1 1 2 3]) '(-1 1 2 3)
+    (drop-while pos? [-1 -2 -3]) '(-1 -2 -3) ))
+
+
+(deftest test-butlast
+  (are (= _1 _2)
+    (butlast []) nil
+    (butlast [1]) nil
+    (butlast [1 2 3]) '(1 2) ))
+
+
+(deftest test-drop-last
+  (are (= _1 _2)
+    ; as butlast
+    (drop-last []) nil
+    (drop-last [1]) nil
+    (drop-last [1 2 3]) '(1 2)
+
+    ; as butlast
+    (drop-last 1 []) nil
+    (drop-last 1 [1]) nil
+    (drop-last 1 [1 2 3]) '(1 2)
+
+    (drop-last 2 []) nil
+    (drop-last 2 [1]) nil
+    (drop-last 2 [1 2 3]) '(1)
+
+    (drop-last 5 []) nil
+    (drop-last 5 [1]) nil
+    (drop-last 5 [1 2 3]) nil
+
+    (drop-last 0 []) nil
+    (drop-last 0 [1]) '(1)
+    (drop-last 0 [1 2 3]) '(1 2 3)
+
+    (drop-last -1 []) nil
+    (drop-last -1 [1]) '(1)
+    (drop-last -1 [1 2 3]) '(1 2 3)
+
+    (drop-last -2 []) nil
+    (drop-last -2 [1]) '(1)
+    (drop-last -2 [1 2 3]) '(1 2 3) ))
+
+
+(deftest test-split-at
+  (is (vector? (split-at 2 [])))
+  (is (vector? (split-at 2 [1 2 3])))
+
+  (are (= _1 _2)
+    (split-at 2 []) [nil nil]
+    (split-at 2 [1 2 3 4 5]) [(list 1 2) (list 3 4 5)]
+
+    (split-at 5 [1 2 3]) [(list 1 2 3) nil]
+    (split-at 0 [1 2 3]) [nil (list 1 2 3)]
+    (split-at -1 [1 2 3]) [nil (list 1 2 3)]
+    (split-at -5 [1 2 3]) [nil (list 1 2 3)] ))
+
+
+(deftest test-split-with
+  (is (vector? (split-with pos? [])))
+  (is (vector? (split-with pos? [1 2 -1 0 3 4])))
+
+  (are (= _1 _2)
+    (split-with pos? []) [nil nil]
+    (split-with pos? [1 2 -1 0 3 4]) [(list 1 2) (list -1 0 3 4)]
+
+    (split-with pos? [-1 2 3 4 5]) [nil (list -1 2 3 4 5)]
+    (split-with number? [1 -2 "abc" \x]) [(list 1 -2) (list "abc" \x)] ))
+
+
+(deftest test-vals
+  (are (= _1 _2)      ; other than map data structures
+      (vals ()) nil
+      (vals []) nil
+      (vals #{}) nil
+      (vals "") nil )
+
+  (are (= _1 _2)
+      ; (class {:a 1}) => clojure.lang.PersistentArrayMap
+      (vals {}) nil
+      (vals {:a 1}) '(1)
+      (diff (vals {:a 1 :b 2}) '(1 2)) nil              ; (vals {:a 1 :b 2}) '(1 2)
+
+      ; (class (sorted-map :a 1)) => clojure.lang.PersistentTreeMap
+      (vals (sorted-map)) nil
+      (vals (sorted-map :a 1)) '(1)
+      (diff (vals (sorted-map :a 1 :b 2)) '(1 2)) nil   ; (vals (sorted-map :a 1 :b 2)) '(1 2)
+
+      ; (class (hash-map :a 1)) => clojure.lang.PersistentHashMap
+      (vals (hash-map)) nil
+      (vals (hash-map :a 1)) '(1)
+      (diff (vals (hash-map :a 1 :b 2)) '(1 2)) nil ))  ; (vals (hash-map :a 1 :b 2)) '(1 2)
+
+
+(deftest test-keys
+  (are (= _1 _2)      ; other than map data structures
+      (keys ()) nil
+      (keys []) nil
+      (keys #{}) nil
+      (keys "") nil )
+
+  (are (= _1 _2)
+      ; (class {:a 1}) => clojure.lang.PersistentArrayMap
+      (keys {}) nil
+      (keys {:a 1}) '(:a)
+      (diff (keys {:a 1 :b 2}) '(:a :b)) nil              ; (keys {:a 1 :b 2}) '(:a :b)
+
+      ; (class (sorted-map :a 1)) => clojure.lang.PersistentTreeMap
+      (keys (sorted-map)) nil
+      (keys (sorted-map :a 1)) '(:a)
+      (diff (keys (sorted-map :a 1 :b 2)) '(:a :b)) nil   ; (keys (sorted-map :a 1 :b 2)) '(:a :b)
+
+      ; (class (hash-map :a 1)) => clojure.lang.PersistentHashMap
+      (keys (hash-map)) nil
+      (keys (hash-map :a 1)) '(:a)
+      (diff (keys (hash-map :a 1 :b 2)) '(:a :b)) nil ))  ; (keys (hash-map :a 1 :b 2)) '(:a :b)
+
+
+(deftest test-empty?
+  (are (empty? _)
+    nil
+    ()
+    []
+    {}
+    #{}
+    ""
+    (into-array []) )
+
+  (are (not (empty? _))
+    '(1 2)
+    [1 2]
+    {:a 1 :b 2}
+    #{1 2}
+    "abc"
+    (into-array [1 2]) ))
+
+
+;; pmap
+;;
+(deftest pmap-does-its-thing
+  ;; regression fixed in r1218; was OutOfMemoryError
+  (is (= '(1) (pmap inc [0]))))
+
+
+;; equality
+;;
+(deftest vectors-equal-other-seqs-by-items-equality
+  ;; regression fixed in r1208; was not equal
+  (is (= '() []))
+  (is (= '(1) [1])))
+
